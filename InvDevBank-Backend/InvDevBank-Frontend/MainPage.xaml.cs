fdcd1adf26c6b539adebe6dc74652bc98919ad9c
Michael Sauerman <michael.shea.sauerman@gmail.com>
2024-06-07 23:24:17 +0200

    func(intialcommit): added intial commit

---
+++ InvDevBank-Backend/InvDevBank-Frontend/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace InvDevBank_Frontend
+{
+    public partial class MainPage : ContentPage
+    {
+        string count = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            HttpClient client = new();
+
+           try
+           {
+               var temp = client.GetAsync("https://localhost:32774/GetWeatherForecast").Result;
+               CounterBtn.Text = temp.ToString();
+            }
+           catch (Exception ex)
+           {
+                CounterBtn.Text = ex.Message;
+           }
+        }
+    }
+
+}
